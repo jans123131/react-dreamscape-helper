4f9048156e73b082779fbe2ed5e98dc04d31c3ed
jans123131 <jans@talelgym.tn>
2025-02-10 22:50:14 +0100

    ok

--- Screens/HomeScreen/services/foodService.js
+++
@@ -1,9 +0,0 @@
-export const fetchFoodItems = async () => {
-    try {
-      const response = await fetch('http://192.168.1.53:5002/api/foods');
-      const data = await response.json();
-      return data;
-    } catch (err) {
-      throw new Error(err.message);
-    }
-  };
