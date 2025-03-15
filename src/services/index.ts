5ef6fe0e0b52ab995d0550c11fdeff6b3ba0a707
jans123131 <jans@talelgym.tn>
2025-03-15 20:02:26 +0100

    OKI

--- src/services/index.ts
+++
@@ -1,9 +0,0 @@
-
-export * from './auth.service';
-export * from './users.service';
-export * from './tasks.service';
-export * from './events.service';
-export * from './artists.service';
-export * from './files.service';
-export * from './passwords.service';
-export * from './invoices.service';
