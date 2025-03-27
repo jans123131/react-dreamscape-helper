2a95cfdf7ee0edf04f1e66565af90ddf9f62f4b4
jans123131 <jans@talelgym.tn>
2025-03-27 15:29:26 +0100

    IKII

--- index.js
+++
@@ -1,7 +0,0 @@
-
-// Import necessary packages for React Native
-import { registerRootComponent } from 'expo';
-import App from './App';
-
-// Register the main component
-registerRootComponent(App);
