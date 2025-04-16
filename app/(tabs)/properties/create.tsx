4e02ba71a4436e0dd15539d8cbc09ffe20c498e5
jans123131 <jans@talelgym.tn>
2025-04-16 19:02:02 +0100

    de

--- app/(tabs)/properties/create.tsx
+++
@@ -1,7 +0,0 @@
-
-import React from 'react';
-import CreatePropertyScreen from '../../../src/pages/owner/CreatePropertyScreen';
-
-export default function CreatePropertyPage() {
-  return <CreatePropertyScreen />;
-}
--- app/(tabs)/properties/index.tsx
+++
@@ -1,7 +0,0 @@
-
-import React from 'react';
-import PropertiesListScreen from '../../../src/pages/owner/PropertiesListScreen';
-
-export default function PropertiesListPage() {
-  return <PropertiesListScreen />;
-}
